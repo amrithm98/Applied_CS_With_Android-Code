6501eb31e61d4d7d893ce7ec7981c10165ee35bc
amrithm98 <amrithchallenger@gmail.com>
2017-06-02 11:25:43 +0530

    Minor changs and user roll function

--- Scarnes-Dice/app/src/main/java/amrith/com/scarnesdice/MainActivity.java
+++ Scarnes-Dice/app/src/main/java/amrith/com/scarnesdice/MainActivity.java
@@ -2,7 +2,11 @@ package amrith.com.scarnesdice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -27,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     String labelText = userScoreLabel + userOverallScore + compScoreLabel + computerOverallScore;
 
     Button reset,hold,roll;
+    TextView status;
+    ImageView diceImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,17 +43,46 @@ public class MainActivity extends AppCompatActivity {
         reset=(Button)findViewById(R.id.btn_reset);
         hold=(Button)findViewById(R.id.btn_hold);
         roll=(Button)findViewById(R.id.btn_roll);
+
+        status=(TextView)findViewById(R.id.statusView);
+
+        diceImage=(ImageView)findViewById(R.id.imageView);
     }
 
-    public void rollClicked(){
+    public void rollClicked(View view){
+
+        Log.d("Click","Roll");
+        int rolledNumber=rollDice();
+        diceImage.setImageResource(drawable[rolledNumber]);
+
+        rolledNumber++;
+
+        if(rolledNumber==1){
+            userTurnScore = 0;
+            labelText = userScoreLabel + userOverallScore + compScoreLabel + computerOverallScore + userTurnScoreLabel + userTurnScore + "\n you lost your chance";
+            computerTurn();
+        }
+        else
+        {
+            userTurnScore=rolledNumber;
+            userOverallScore+=userTurnScore;
+            labelText = userScoreLabel + userOverallScore + compScoreLabel + computerOverallScore + userTurnScoreLabel + userTurnScore;
+        }
+
+        status.setText(labelText);
 
     }
 
-    public void holdClicked(){
+    public void holdClicked(View view){
+        Log.d("Click","Hold");
+
 
     }
 
-    public void resetClicked(){
+    public void resetClicked(View view){
+
+        Log.d("Click","Reset");
+
 
         userTurnScore=0;
         userOverallScore=0;
