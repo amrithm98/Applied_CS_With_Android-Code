ae5fff896518fa42ea3d02c73b3bdbd4ff7b2f10
amrithm98 <amrithchallenger@gmail.com>
2017-06-02 11:08:20 +0530

    Setting Up

--- Scarnes-Dice/app/src/main/java/amrith/com/scarnesdice/MainActivity.java
+++ Scarnes-Dice/app/src/main/java/amrith/com/scarnesdice/MainActivity.java
@@ -2,6 +2,7 @@ package amrith.com.scarnesdice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import java.util.Random;
 
@@ -9,10 +10,56 @@ public class MainActivity extends AppCompatActivity {
 
     public int userTurnScore=0,userOverallScore=0,computerTurnScore=0,computerOverallScore=0;
 
+    public int [] drawable={
+            R.drawable.dice1,
+            R.drawable.dice2,
+            R.drawable.dice3,
+            R.drawable.dice4,
+            R.drawable.dice5,
+            R.drawable.dice6
+    };
+
+    String userScoreLabel = "Your Score : ";
+    String compScoreLabel = " Computer Score : ";
+    String userTurnScoreLabel = " Your Turn Score : ";
+    String compTurnScoreLabel = "\nComputer Turn Score : ";
+
+    String labelText = userScoreLabel + userOverallScore + compScoreLabel + computerOverallScore;
+
+    Button reset,hold,roll;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        reset=(Button)findViewById(R.id.btn_reset);
+        hold=(Button)findViewById(R.id.btn_hold);
+        roll=(Button)findViewById(R.id.btn_roll);
+    }
+
+    public void rollClicked(){
+
+    }
+
+    public void holdClicked(){
+
+    }
+
+    public void resetClicked(){
+
+        userTurnScore=0;
+        userOverallScore=0;
+        computerTurnScore=0;
+        computerOverallScore=0;
+
+        labelText = userScoreLabel + userOverallScore + compScoreLabel + computerOverallScore;
+        enableButtons(true);
+    }
+
+    public void computerTurn(){
+
     }
 
     private int rollDice() {
@@ -24,7 +71,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void enableButtons(boolean isEnabled) {
-//        rollButton.setEnabled(isEnabled);
-//        holdButton.setEnabled(isEnabled);
+        roll.setEnabled(isEnabled);
+        hold.setEnabled(isEnabled);
     }
+
+
 }
