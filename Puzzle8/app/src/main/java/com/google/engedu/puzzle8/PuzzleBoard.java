969aa544a0c973b872d5bce86cfec255ac3ef3ae
amrithm98 <amrithchallenger@gmail.com>
2017-06-20 13:12:58 +0530

    puzzle 8

---
+++ Puzzle8/app/src/main/java/com/google/engedu/puzzle8/PuzzleBoard.java
@@ -0,0 +1,118 @@
+/* Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.engedu.puzzle8;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+import java.util.ArrayList;
+
+
+public class PuzzleBoard {
+
+    private static final int NUM_TILES = 3;
+    private static final int[][] NEIGHBOUR_COORDS = {
+            { -1, 0 },
+            { 1, 0 },
+            { 0, -1 },
+            { 0, 1 }
+    };
+    private ArrayList<PuzzleTile> tiles;
+
+    PuzzleBoard(Bitmap bitmap, int parentWidth) {
+    }
+
+    PuzzleBoard(PuzzleBoard otherBoard) {
+        tiles = (ArrayList<PuzzleTile>) otherBoard.tiles.clone();
+    }
+
+    public void reset() {
+        // Nothing for now but you may have things to reset once you implement the solver.
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null)
+            return false;
+        return tiles.equals(((PuzzleBoard) o).tiles);
+    }
+
+    public void draw(Canvas canvas) {
+        if (tiles == null) {
+            return;
+        }
+        for (int i = 0; i < NUM_TILES * NUM_TILES; i++) {
+            PuzzleTile tile = tiles.get(i);
+            if (tile != null) {
+                tile.draw(canvas, i % NUM_TILES, i / NUM_TILES);
+            }
+        }
+    }
+
+    public boolean click(float x, float y) {
+        for (int i = 0; i < NUM_TILES * NUM_TILES; i++) {
+            PuzzleTile tile = tiles.get(i);
+            if (tile != null) {
+                if (tile.isClicked(x, y, i % NUM_TILES, i / NUM_TILES)) {
+                    return tryMoving(i % NUM_TILES, i / NUM_TILES);
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean tryMoving(int tileX, int tileY) {
+        for (int[] delta : NEIGHBOUR_COORDS) {
+            int nullX = tileX + delta[0];
+            int nullY = tileY + delta[1];
+            if (nullX >= 0 && nullX < NUM_TILES && nullY >= 0 && nullY < NUM_TILES &&
+                    tiles.get(XYtoIndex(nullX, nullY)) == null) {
+                swapTiles(XYtoIndex(nullX, nullY), XYtoIndex(tileX, tileY));
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    public boolean resolved() {
+        for (int i = 0; i < NUM_TILES * NUM_TILES - 1; i++) {
+            PuzzleTile tile = tiles.get(i);
+            if (tile == null || tile.getNumber() != i)
+                return false;
+        }
+        return true;
+    }
+
+    private int XYtoIndex(int x, int y) {
+        return x + y * NUM_TILES;
+    }
+
+    protected void swapTiles(int i, int j) {
+        PuzzleTile temp = tiles.get(i);
+        tiles.set(i, tiles.get(j));
+        tiles.set(j, temp);
+    }
+
+    public ArrayList<PuzzleBoard> neighbours() {
+        return null;
+    }
+
+    public int priority() {
+        return 0;
+    }
+
+}
---
+++ Puzzle8/app/src/main/java/com/google/engedu/puzzle8/PuzzleBoardView.java
@@ -0,0 +1,96 @@
+/* Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.engedu.puzzle8;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class PuzzleBoardView extends View {
+    public static final int NUM_SHUFFLE_STEPS = 40;
+    private Activity activity;
+    private PuzzleBoard puzzleBoard;
+    private ArrayList<PuzzleBoard> animation;
+    private Random random = new Random();
+
+    public PuzzleBoardView(Context context) {
+        super(context);
+        activity = (Activity) context;
+        animation = null;
+    }
+
+    public void initialize(Bitmap imageBitmap) {
+        int width = getWidth();
+        puzzleBoard = new PuzzleBoard(imageBitmap, width);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (puzzleBoard != null) {
+            if (animation != null && animation.size() > 0) {
+                puzzleBoard = animation.remove(0);
+                puzzleBoard.draw(canvas);
+                if (animation.size() == 0) {
+                    animation = null;
+                    puzzleBoard.reset();
+                    Toast toast = Toast.makeText(activity, "Solved! ", Toast.LENGTH_LONG);
+                    toast.show();
+                } else {
+                    this.postInvalidateDelayed(500);
+                }
+            } else {
+                puzzleBoard.draw(canvas);
+            }
+        }
+    }
+
+    public void shuffle() {
+        if (animation == null && puzzleBoard != null) {
+            // Do something. Then:
+            puzzleBoard.reset();
+            invalidate();
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (animation == null && puzzleBoard != null) {
+            switch(event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    if (puzzleBoard.click(event.getX(), event.getY())) {
+                        invalidate();
+                        if (puzzleBoard.resolved()) {
+                            Toast toast = Toast.makeText(activity, "Congratulations!", Toast.LENGTH_LONG);
+                            toast.show();
+                        }
+                        return true;
+                    }
+            }
+        }
+        return super.onTouchEvent(event);
+    }
+
+    public void solve() {
+    }
+}
