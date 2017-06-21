5b0d1dcdd56c2a84c03a85103a471915e1c81f25
amrithm98 <amrithchallenger@gmail.com>
2017-06-21 12:09:38 +0530

    Word Ladder complete...Working good

--- WordLadder_starter/app/src/main/java/com/google/engedu/wordladder/SolverActivity.java
+++ WordLadder_starter/app/src/main/java/com/google/engedu/wordladder/SolverActivity.java
@@ -5,9 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.engedu.worldladder.R;
 
@@ -18,17 +20,34 @@ public class SolverActivity extends AppCompatActivity {
     TextView startText,endText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_solver);
         Intent intent=getIntent();
         Bundle data=intent.getExtras();
-        linearLayout=(LinearLayout)findViewById(R.id.ll_editTexts);
         words=data.getStringArray("words");
-        editTexts=new EditText[words.length];
-        for(int i=0;i<words.length;i++)
+
+        linearLayout=(LinearLayout)findViewById(R.id.ll_editTexts);
+        startText=(TextView)findViewById(R.id.startTextView);
+        endText=(TextView)findViewById(R.id.endTextView);
+        editTexts=new EditText[words.length-2];
+
+        startText.setText(words[0].toString());
+        endText.setText(words[words.length-1].toString());
+        //1 to length-1 ... First and last words are already there
+        for(int i=1;i<words.length-1;i++)
         {
-            editTexts[i]=new EditText(SolverActivity.this);
-            linearLayout.addView(editTexts[i]);
+            editTexts[i-1]=new EditText(SolverActivity.this);
+            linearLayout.addView(editTexts[i-1]);
         }
+    }
+
+    public void onSolve(View view)
+    {
+        for(int i=1;i<words.length-1;i++)
+        {
+            editTexts[i-1].setText(words[i]);
+        }
+        Toast.makeText(this,"Solved",Toast.LENGTH_SHORT).show();
     }
 }
